dd7934116cf25887c6f079bb6baa78b44601953c
MrKevinWeiss <weiss.kevin604@gmail.com>
2023-08-31 14:23:27 +0200

    wip: inventory changes

--- src/inet_nm/cli_inventory.py
+++ src/inet_nm/cli_inventory.py
@@ -1,5 +1,7 @@
 import argparse
+import hashlib
 import json
+import tempfile
 
 import inet_nm.check as chk
 import inet_nm.config as cfg
@@ -23,6 +25,72 @@ def main():
     m_boards = chk.nodes_to_boards(missing)
     t_boards = chk.nodes_to_boards(total)
 
+    # Possibly assert uids are unique for each available, used, missing
+    uid_states = {}
+    uid_states.update({node.uid: "available" for node in available})
+    uid_states.update({node.uid: "used" for node in used})
+    uid_states.update({node.uid: "missing" for node in missing})
+
+    # get hash of tempfile name based on config path
+    cfg_path = args.config
+    cfg_hash = hashlib.md5(str(cfg_path).encode()).hexdigest()
+    tmp_state_path = (
+        tempfile.gettempdir() + "/" + "inet-nm-uid-states-" + cfg_hash + ".json"
+    )
+    tmp_changes_path = (
+        tempfile.gettempdir() + "/" + "inet-nm-changes-" + cfg_hash + ".json"
+    )
+
+    # Try to read from tempfile if it exists for old uid_states
+    try:
+        with open(tmp_state_path, "r") as f:
+            old_uid_states = json.load(f)
+    except FileNotFoundError:
+        old_uid_states = {}
+
+    # If any old states differ from new states add to a changes state dict
+    changes = {}
+    board_changes = {}
+    new_uids = set(uid_states.keys()) - set(old_uid_states.keys())
+    for uid in new_uids:
+        changes[uid] = uid_states[uid]
+        # get board from uid
+        board = [node.board for node in total if node.uid == uid][0]
+        b_key = f"{board}_{uid_states[uid]}"
+        if b_key not in board_changes:
+            board_changes[b_key] = 0
+        board_changes[b_key] += 1
+
+    x_uids = set(uid_states.keys()).intersection(set(old_uid_states.keys()))
+    for uid in x_uids:
+        if uid_states[uid] != old_uid_states[uid]:
+            changes[uid] = uid_states[uid]
+            # get board from uid
+            board = [node.board for node in total if node.uid == uid][0]
+            b_key = f"{board}_{uid_states[uid]}"
+            if b_key not in board_changes:
+                board_changes[b_key] = 0
+            board_changes[f"{board}_{uid_states[uid]}"] += 1
+            b_key = f"{board}_{old_uid_states[uid]}"
+            if b_key not in board_changes:
+                board_changes[b_key] = 0
+            board_changes[f"{board}_{old_uid_states[uid]}"] -= 1
+
+    # Write new uid_states to tempfile
+    if changes:
+        with open(tmp_state_path, "w") as f:
+            json.dump(uid_states, f, sort_keys=True, indent=2)
+            print(f"Updated uid_states written to {tmp_state_path}")
+        with open(tmp_changes_path, "w") as f:
+            json.dump(board_changes, f, sort_keys=True, indent=2)
+            print(f"Updated changes written to {tmp_changes_path}")
+    else:
+        try:
+            with open(tmp_changes_path, "r") as f:
+                board_changes = json.load(f)
+        except FileNotFoundError:
+            board_changes = {}
+
     boards = sorted(list(set(t_boards.keys())))
 
     info = [
@@ -32,6 +100,9 @@ def main():
             "used": u_boards.get(key, 0),
             "missing": m_boards.get(key, 0),
             "total": t_boards.get(key, 0),
+            "available_changes": board_changes.get(f"{key}_available", 0),
+            "used_changes": board_changes.get(f"{key}_used", 0),
+            "missing_changes": board_changes.get(f"{key}_missing", 0),
         }
         for key in boards
     ]
@@ -56,9 +127,24 @@ def main():
         total_count = 0
         for i in info:
             brd_str = f"| {i['board']:<{max_board_len + 1}}"
-            avl_str = f"| {i['available']:>9} "
-            use_str = f"| {i['used']:>9} "
-            mis_str = f"| {i['missing']:>9} "
+            state = " "
+            if i["available_changes"] > 0:
+                state = "+"
+            elif i["available_changes"] < 0:
+                state = "-"
+            avl_str = f"|{state}{i['available']:>9} "
+            state = " "
+            if i["used_changes"] > 0:
+                state = "+"
+            elif i["used_changes"] < 0:
+                state = "-"
+            use_str = f"|{state}{i['used']:>9} "
+            state = " "
+            if i["missing_changes"] > 0:
+                state = "+"
+            elif i["missing_changes"] < 0:
+                state = "-"
+            mis_str = f"|{state}{i['missing']:>9} "
             tot_str = f"| {i['total']:>9} |"
             full_str = brd_str + avl_str + use_str + mis_str + tot_str
             print(full_str)
